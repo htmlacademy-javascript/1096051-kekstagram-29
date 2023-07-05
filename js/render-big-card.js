ee506b0009c1f2e315b0c4cd97c8737fc481b7c8
problemon <danilets.pasha@mail.ru>
2023-07-05 11:29:44 +1000

    Refactoring

--- js/render-big-card.js
+++ js/render-big-card.js
@@ -4,24 +4,72 @@ const bigCard = document.querySelector('.big-picture');
 const closeButton = bigCard.querySelector('.big-picture__cancel');
 const commentsList = bigCard.querySelector('.social__comments');
 const commentTemplate = commentsList.querySelector('.social__comment');
+const commentCount = bigCard.querySelector('.social__comment-count');
+const buttonLoadComments = bigCard.querySelector('.comments-loader');
+
+const getCountComments = (commentsElements) => {
+  let countComments = 5;
+  if (countComments > commentsElements.length) {
+    countComments = commentsElements.length;
+  }
+  return countComments;
+};
+
+const getCountUnhiddenComments = (startIndex, commentsElements) => {
+  let loadedComments = startIndex + getCountComments(commentsElements);
+  if (loadedComments > commentsElements.length) {
+    loadedComments = commentsElements.length;
+  }
+  return loadedComments;
+};
+
+const getCountShowedComments = (commentsElements) => {
+  const hiddenComments = commentsList.querySelectorAll('.social__comment.hidden');
+  return commentsElements.length - hiddenComments.length;
+};
+
+const showCommentsInRange = (startIndex, countUnhiddenComments, commentsElements) => {
+  for (let i = startIndex; i < countUnhiddenComments; i++) {
+    commentsElements[i].classList.remove('hidden');
+  }
+};
+
+const renderTextCountComments = (countUnhiddenComments, commentsElements) => {
+  commentCount.innerHTML = `${countUnhiddenComments} из <span class="comments-count">${commentsElements.length}</span> комментариев`
+};
+
+const loadComments = () => {
+  const commentsElements = commentsList.querySelectorAll('.social__comment');
+  const startIndex = getCountShowedComments(commentsElements); // индекс начала цыкла относительно первого скрытого комментария.
+  const countUnhiddenComments = getCountUnhiddenComments(startIndex, commentsElements); // количество комментариев которое нужно открыть.
+
+  showCommentsInRange(startIndex, countUnhiddenComments, commentsElements);
+  renderTextCountComments(countUnhiddenComments, commentsElements);
+};
+
+const onButtonLoadCommentsClick = () => {
+  loadComments();
+};
+
+const renderComment = ({avatar, message, name}) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  const commentAvatar = commentElement.querySelector('.social__picture');
+  const commentText = commentElement.querySelector('.social__text');
+
+  commentAvatar.src = avatar;
+  commentAvatar.alt = name;
+  commentText.textContent = message;
+
+  commentElement.classList.add('hidden');
+
+  return commentElement;
+};
 
 const renderComments = (comments) => {
-  commentsList.innerHTML = '';
   const commentsListFragment = document.createDocumentFragment();
+  comments.forEach((element) => commentsListFragment.append(renderComment(element)));
 
-  comments.forEach(({avatar, message, name}) => {
-    const commentElement = commentTemplate.cloneNode(true);
-    const commentAvatar = commentElement.querySelector('.social__picture');
-    const commentText = commentElement.querySelector('.social__text');
-
-    commentAvatar.src = avatar;
-    commentAvatar.alt = name;
-    commentText.textContent = message;
-
-
-    commentsListFragment.append(commentElement);
-  });
-
+  commentsList.innerHTML = '';
   commentsList.append(commentsListFragment);
 };
 
@@ -30,9 +78,6 @@ const renderBigCard = ({url, description, likes, comments}) => {
   bigCard.querySelector('.likes-count').textContent = likes;
   bigCard.querySelector('.comments-count').textContent = comments.length;
   bigCard.querySelector('.social__caption').textContent = description;
-
-  bigCard.querySelector('.social__comment-count').classList.add('hidden');
-  bigCard.querySelector('.comments-loader').classList.add('hidden');
 
   renderComments(comments);
 };
@@ -50,15 +95,18 @@ function closeBigCard () {
 
   document.removeEventListener('keydown', onKeyDown);
   closeButton.removeEventListener('click', closeBigCard);
+  buttonLoadComments.removeEventListener('click', onButtonLoadCommentsClick);
 }
 
 function openBigCard (cardData) {
   bigCard.classList.remove('hidden');
   document.body.classList.add('modal-open');
   renderBigCard(cardData);
+  loadComments();
 
   document.addEventListener('keydown', onKeyDown);
   closeButton.addEventListener('click', closeBigCard);
+  buttonLoadComments.addEventListener('click', onButtonLoadCommentsClick);
 }
 
 
--- js/render.js
+++ js/render.js
@@ -6,12 +6,10 @@ const pictureElement = pictureTemplate.querySelector('.picture');
 
 const picturesFragment = document.createDocumentFragment();
 
-const createPicture = (cardData) => {
-  const { url, description, likes, comments } = cardData;
-
+const createPicture = ({url, description, likes, comments, id}) => {
   const picture = pictureElement.cloneNode(true);
-  picture.addEventListener('click', () => openBigCard(cardData));
 
+  picture.id = id;
   picture.querySelector('.picture__img').src = url;
   picture.querySelector('.picture__img').alt = description;
   picture.querySelector('.picture__likes').textContent = likes;
@@ -20,12 +18,33 @@ const createPicture = (cardData) => {
   return picture;
 };
 
+const getCardDataFromId = (pictureId, cardsData) => {
+  for (let i = 0; i < cardsData.length; i++) {
+    const dataId = Number(cardsData[i].id);
+    const elementId = Number(pictureId.id);
+
+    if (elementId === dataId) {
+      return cardsData[i];
+    }
+  }
+};
+
+const onPictureElementClick = (evt, cardsData) => {
+  const targetPictureElement = evt.target.closest('.picture');
+
+  if (targetPictureElement) {
+    const cardData = getCardDataFromId(targetPictureElement, cardsData);
+    openBigCard(cardData);
+  }
+};
+
 const renderPicture = (cardsData) => {
   cardsData.forEach((cardData) => {
     picturesFragment.append(createPicture(cardData));
   });
 
   picturesList.append(picturesFragment);
+  picturesList.addEventListener('click', (evt) => onPictureElementClick(evt, cardsData));
 };
 
 export { renderPicture };
